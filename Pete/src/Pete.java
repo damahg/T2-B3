5e95efc323f8e3f87b1ced3070ffc1ac682438df
DAM-1 <DAM-1@PC23>
2022-02-24 13:38:10 +0100

    .

--- Pete/src/Pete.java
+++ Pete/src/Pete.java
@@ -2,8 +2,20 @@
 public class Pete {
 
 	public static void main(String[] args) {
-		int[] pete = new int[Integer.MAX_VALUE];
-
+		int[] pete = new int[(Integer.MAX_VALUE/5)];
+		llenar(pete);
+		llenar2(pete);
+	}
+	
+	public static void llenar(int[] pete) {
+		for (int i = 0; i < pete.length; i++) {
+			pete[i] = Integer.MAX_VALUE;
+		}
+	}
+	
+	public static void llenar2(int[] pete) {
+		for (int i = 0; i < pete.length; i++) {
+			System.out.println(pete[i]);
 	}
 
 }
